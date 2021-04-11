0c229cf2e16ea01bbf8472bfa1a00cb308bbb6b8
bakaiismailov <ismailov.time@gmail.com>
2021-04-11 13:50:08 +0600

    first one

---
+++ app/src/main/java/com/hfad/ad4lesson1/MainActivity.kt
@@ -0,0 +1,48 @@
+package com.hfad.ad4lesson1
+import android.app.Activity
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import androidx.activity.result.ActivityResult
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import com.hfad.ad4lesson1.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var ui: ActivityMainBinding
+    private lateinit var startForResult: ActivityResultLauncher<Intent>
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        ui = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(ui.root)
+
+        startForResult =
+            registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result: ActivityResult ->
+                if (result.resultCode == Activity.RESULT_OK) {
+                    val intent = result.data
+                    ui.editText.setText(intent?.getStringExtra("res"))
+                }
+            }
+
+        ui.button.setOnClickListener{
+            if (!ui.editText.text.isNullOrBlank()){
+
+                val intent = Intent(this, SecondActivity::class.java)
+                intent.putExtra("res", ui.editText.text?.toString())
+                startForResult.launch(intent)
+
+            } else {
+                showMessage()
+            }
+        }
+    }
+
+    private fun showMessage() {
+        Toast.makeText(this, "Field cannot be empty", Toast.LENGTH_SHORT).show()
+        Log.e("TAG", "showMessage:  field is empty")
+    }
+}
---
+++ app/src/main/java/com/hfad/ad4lesson1/SecondActivity.kt
@@ -0,0 +1,45 @@
+package com.hfad.ad4lesson1
+import android.app.Activity
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import com.hfad.ad4lesson1.databinding.ActivitySecondBinding
+
+class SecondActivity : AppCompatActivity() {
+    private lateinit var ui: ActivitySecondBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        ui = ActivitySecondBinding.inflate(layoutInflater)
+        setContentView(ui.root)
+
+        val data: String? = intent.getStringExtra("res").toString()
+        data?.let { ui.editText.setText(data) }
+
+        ui.button.setOnClickListener {
+
+            if (!ui.editText.text.isNullOrBlank()) {
+
+                val intent = Intent()
+                intent.putExtra("res", ui.editText.text?.toString())
+                setResult(Activity.RESULT_OK, intent)
+                finish()
+
+            } else {
+                showMessage()
+            }
+        }
+    }
+
+    override fun onBackPressed() {
+        setResult(Activity.RESULT_CANCELED)
+        finish()
+    }
+
+    private fun showMessage() {
+        Toast.makeText(this, "Field cannot be empty", Toast.LENGTH_SHORT).show()
+        Log.e("TAG", "showMessage:  field is empty")
+    }
+}
